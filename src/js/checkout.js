8b649fbcf3f3262ea7ace2945ce031375dcd239b
isiakponachuks <isi21001@byui.edu>
2022-05-19 23:38:52 +0100

    vkkfjfj

---
+++ src/js/checkout.js
@@ -0,0 +1,15 @@
+import { loadHeaderFooter } from './utils.js';
+import CheckoutProcess from './CheckoutProcess.js';
+
+loadHeaderFooter();
+
+const myCheckout = new CheckoutProcess ('so-cart', '.checkout-summary');
+myCheckout.init();
+
+document.querySelector('#zip').addEventListener('blur', myCheckout.CalculateOrdertotal.bind(myCheckout));
+document.querySelector('#checkoutSubmit').addEventListener ('click', (e) => {
+    e.preventDefault();
+
+    myCheckout.myCheckout();
+
+});
